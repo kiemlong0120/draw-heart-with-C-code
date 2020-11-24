65a08a5ca80a45894120a5824cf75e9325562ec1
kiemlong0120 <vanquag99k@gmail.com>
2020-11-24 11:39:45 +0700

    first commit

---
+++ Vẽ Hình Trái Tym/Vẽ Hình Trái Tym/Source.cpp
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	printf("\n        _##### _   _ #####_");
+	printf("\n      ##        ###        ##");
+	printf("\n     ##          #          ##");
+	printf("\n     ##                     ##");
+	printf("\n     ##      >>GAURED<<     ##");
+	printf("\n      ##     BABY BEAR     ##");
+	printf("\n       ##    ~ QUANG ~    ##");
+	printf("\n         ##             ##");
+	printf("\n           ##         ##");
+	printf("\n             ##     ##");
+	printf("\n               ## ##");
+	printf("\n                 #\n\n");
+getch();
+}
